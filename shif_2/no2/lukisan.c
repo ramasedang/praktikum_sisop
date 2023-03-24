68175d5c31d6ac09fc4a2aa471a66aabc170f26e
aloybm <manullangaloysius@gmail.com>
2023-03-24 19:52:36 +0700

    fix

--- shif_2/no2/lukisan.c
+++ shif_2/no2/lukisan.c
@@ -1,36 +1,59 @@
+#include <sys/stat.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <string.h>
 #include <time.h>
-#include <fcntl.h>
-#include <errno.h>
+
 #include <dirent.h>
-#include <wait.h>
 #include <signal.h>
 #include <stdbool.h>
 
-
-#define NUM_IMAGES 15
-#define INTERVAL 30
 #define MODE_A 1
 #define MODE_B 2
 
 bool is_running = true;
 int mode;
 
-void download_image(const char* folder_name, int thread_num) {
+
+//Handler untuk sinyal SIGTERM
+void sigterm_handler(int sig) {
+    is_running = false;
+}
+
+//fungsi menghapus folder
+void delete_folder(const char* nama_folder) {
+    sleep(5);
+    pid_t pid = fork();
+    if (pid == 0) {
+        execl("/bin/rm", "rm", "-rf", nama_folder, NULL);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+//fungsi melakukan zip pada folder
+void zip_folder(const char* nama_folder) {
+    char nama_zip[100];
+    sprintf(nama_zip, "%s.zip", nama_folder);
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        execl("/usr/bin/zip", "zip", "-rmq", nama_zip, nama_folder, NULL);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+//fungsi mendownload gambar sesuai perintah pada soal
+void download_gambar(const char* nama_folder, int thread_num) {
     time_t t = time(NULL);
-    char img_name[50];
-    sprintf(img_name, "%04d-%02d-%02d_%02d:%02d:%02d_%d.jpg",
+    char nama_gambar[50];
+    sprintf(nama_gambar, "%04d-%02d-%02d_%02d:%02d:%02d_%d.jpg",
         1900 + localtime(&t)->tm_year, localtime(&t)->tm_mon + 1, localtime(&t)->tm_mday,
         localtime(&t)->tm_hour, localtime(&t)->tm_min, localtime(&t)->tm_sec, thread_num);
     char url[50];
     sprintf(url, "https://picsum.photos/%d", (int) (t%1000)+50);
     char path[100];
-    sprintf(path, "%s/%s", folder_name, img_name);
+    sprintf(path, "%s/%s", nama_folder, nama_gambar);
 
     pid_t pid = fork();
     if (pid == 0) {
@@ -39,43 +62,23 @@ void download_image(const char* folder_name, int thread_num) {
     }
 }
 
-void zip_folder(const char* folder_name) {
-    char zip_name[100];
-    sprintf(zip_name, "%s.zip", folder_name);
-    pid_t pid = fork();
-    if (pid == 0) {
-        execl("/usr/bin/zip", "zip", "-rmq", zip_name, folder_name, NULL);
-        exit(EXIT_SUCCESS);
-    }
-}
-
-void delete_folder(const char* folder_name) {
-    sleep (8);
-    pid_t pid = fork();
-    if (pid == 0) {
-        execl("/bin/rm", "rm", "-rf", folder_name, NULL);
-        exit(EXIT_SUCCESS);
-    }
-}
-
-void create_killer(char* program_name) {
+//fungsi membuat file killer sesuai dengan Modenya
+void create_killer(char* nama_program) {
     FILE* fp;
-    fp = fopen("killer.sh", "w");
+    fp = fopen("killer", "w");
     fprintf(fp, "#!/bin/bash\n");
 
+    //mengecek mode yang digunakan
     if (mode == MODE_A) {
-        fprintf(fp, "killall -SIGKILL %s\n", program_name);
+        fprintf(fp, "killall -SIGKILL %s\n", nama_program);
     } else {
         fprintf(fp, "kill -SIGTERM %d\n", getpid());
     }
 
+    //menghapus file killer setelah digunakan
     fprintf(fp, "rm killer\n");
     fclose(fp);
     chmod("killer", 0700);
-}
-
-void sigterm_handler(int sig) {
-    is_running = false;
 }
 
 int main(int argc, char** argv) {
@@ -84,6 +87,7 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
+    //mengecek mode yang yang digunakan
     if (strcmp(argv[1], "-a") == 0) {
         mode = MODE_A;
     } else if (strcmp(argv[1], "-b") == 0) {
@@ -95,22 +99,25 @@ int main(int argc, char** argv) {
 
     signal(SIGTERM, sigterm_handler);
 
+    // Variabel untuk menyimpan PID dan SID
     pid_t pid, sid;
 
-    // fork parent process
+    // Menyimpan PID dari Child Process
     pid = fork();
 
-       // error handling
+    /* Keluar saat fork gagal
+    * (nilai variabel pid < 0) */
     if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
-    // exit parent process
+    /* Keluar saat fork berhasil
+    * (nilai variabel pid adalah PID dari child process) */
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
-    // change file mode mask
+    // mengubah file mode mask
     umask(0);
 
     // create new session
@@ -120,7 +127,7 @@ int main(int argc, char** argv) {
     }
 
     // change working directory
-    if (chdir("/") < 0) {
+    if (chdir(".") < 0) {
         exit(EXIT_FAILURE);
     }
 
@@ -129,40 +136,38 @@ int main(int argc, char** argv) {
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
+    create_killer(argv[0]);
+
     // start the main loop
     while (is_running) {
         time_t t = time(NULL);
-        char folder_name[50];
-        sprintf(folder_name, "%04d-%02d-%02d_%02d:%02d:%02d", 
+        char nama_folder[50];
+        sprintf(nama_folder, "%04d-%02d-%02d_%02d:%02d:%02d", 
             1900 + localtime(&t)->tm_year, localtime(&t)->tm_mon + 1, localtime(&t)->tm_mday,
             localtime(&t)->tm_hour, localtime(&t)->tm_min, localtime(&t)->tm_sec);
 
         pid_t child_pid = fork();
 
         if (child_pid == 0) {
-            // create folder
-            mkdir(folder_name, 0777);
+            // membuat folder
+            mkdir(nama_folder, 0777);
 
-            // download images
-            for (int i = 0; i < NUM_IMAGES; i++) {
-                download_image(folder_name, i + 1);
-                sleep(1);
+            // download gambar
+            for (int i = 0; i < 15; i++) {
+                download_gambar(nama_folder, i + 1);
+                sleep(5);
             }
 
             // zip folder
-            zip_folder(folder_name);
+            zip_folder(nama_folder);
 
-            // delete folder
-            delete_folder(folder_name);
+            // menghapus folder
+            delete_folder(nama_folder);
 
             exit(EXIT_SUCCESS);
         } else {
-            sleep(INTERVAL);
+            sleep(30);
         }
     }
-
-    // create the killer script
-    create_killer(argv[0]);
-
     exit(EXIT_SUCCESS);
 }
