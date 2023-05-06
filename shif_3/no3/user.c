cc20eb6dab291fe62f1b0074cd17d47d9348e8f8
Moh. Sulthan Arief Rahmatullah <76695790+ramasedang@users.noreply.github.com>
2023-05-07 00:23:37 +0700

    Update user.c

--- shif_3/no3/user.c
+++ shif_3/no3/user.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-#define MSG_SIZE 256
 
 typedef struct {
     long msg_type;
@@ -15,18 +14,12 @@ typedef struct {
     char user_id[10];
 } message;
 
-int msgid;  // Make it global so it can be accessed from the signal handler
+void sigintHandler(int sig_num);
+#define MSG_SIZE 256
+
+
+int msgid;  
 message msg;
-
-void sigintHandler(int sig_num) {
-    // Send an "EXIT" message to the stream
-    strcpy(msg.msg_text, "EXIT");
-    msgsnd(msgid, &msg, sizeof(msg), 0);
-
-    // Terminate the program
-    exit(0);
-}
-
 int main() {
     key_t key = ftok("/home/ssudo/Matkul/sisop-modul3/song-playlist.json", 65);
 
@@ -34,19 +27,19 @@ int main() {
     msg.msg_type = 1;
     sprintf(msg.user_id, "%d", getpid());
 
-    // Register the signal handler for SIGINT (Ctrl+C)
+   
     signal(SIGINT, sigintHandler);
 
-    //send msg start to stream
+   
     strcpy(msg.msg_text, "START");
     msgsnd(msgid, &msg, sizeof(msg), 0);
 
     while (1) {
-        printf("Enter command: ");
+        printf("Masukkan perintah disini:");
         fgets(msg.msg_text, MSG_SIZE, stdin);
         msg.msg_text[strcspn(msg.msg_text, "\n")] = 0;
 
-        // If the command is "EXIT", we should also stop this program
+       
         if (strcmp(msg.msg_text, "EXIT") == 0) {
             msgsnd(msgid, &msg, sizeof(msg), 0);
             break;
@@ -56,4 +49,13 @@ int main() {
     }
 
     return 0;
+}
+
+void sigintHandler(int sig_num) {
+    
+    strcpy(msg.msg_text, "EXIT");
+    msgsnd(msgid, &msg, sizeof(msg), 0);
+
+ 
+    exit(0);
 }
