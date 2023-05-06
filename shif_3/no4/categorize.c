35d2cdd4761419a1664c43d8b7910ff9c49465d5
reyhanqb <107137535+reyhanqb@users.noreply.github.com>
2023-05-07 00:27:40 +0700

    fix

--- shif_3/no4/categorize.c
+++ shif_3/no4/categorize.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <libgen.h>
 #include <string.h>
 #include <dirent.h>
-#include <pthread.h>
+#include <stdio.h>
 #include <ctype.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <fnmatch.h>
-#include <errno.h>
+#include <pthread.h>
 
 #define extensions 100
 #define extensionsLength 10
@@ -18,108 +21,125 @@ char *pathToCategorized = "/home/reyhanqb/Documents/Shift 3/soal4";
 char *currentPath = "/home/reyhanqb/Documents/Shift 3/soal4/files"; 
 
 struct dirent *entry;
-char prevPath[1000], newPath[1000], filenames[1000];
-char ext_list[extensions][extensionsLength]; 
+char prevPath[1000], newPath[1000], filenames[1000], sub_currentPath[1000];
+char extensionsLists[extensions][extensionsLength]; 
 int ext_count = 0;
-char prevPath[1000], newPath[1000], sub_currentPath[1000];
-char filenames[1000];
+
+void categorizeFiles(char *currentPath, char extensionsLists[extensions][extensionsLength], int ext_count) {
+    DIR *dir = opendir(currentPath);
+    int iterations = 0;
+    int i = 0;
+
+    if((entry = readdir(dir)) == NULL){
+        printf("Direktori kosong");
+    }
+    
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, "files") == 0 ) {
+                continue;
+            }
+            sprintf(sub_currentPath, "%s/%s", currentPath, entry->d_name);
+            categorizeFiles(sub_currentPath, extensionsLists, ext_count); 
+        } else if (entry->d_type == DT_REG) {
+            // Check kalau entry ke direktori benar
+            char* filename = entry->d_name;
+
+            // ubah nama file ke lowercase
+            int j = 0;
+            while (filename[j] != '\0') {
+                filenames[j] = tolower(filename[j]);
+                j++;
+            }
+
+            filenames[strlen(filename)] = '\0';
+            while (i < ext_count) {
+                int result = mkdir(extensionsLists[i], 0777);
+                char ext[extensionsLength + 2];
+                sprintf(ext, "*.%s", extensionsLists[i]);
+                            
+                if (fnmatch(ext, filenames, 0) == 0) {
+                    sprintf(prevPath, "%s/%s", currentPath, filename); 
+                    // Tentukan path tujuan file dari dir files 
+                    sprintf(newPath, "%s/%s/%s/%s", pathToCategorized, "categorized", extensionsLists[i], filename); 
+                    rename(prevPath, newPath); 
+                    iterations = 1;
+                    break;
+                }
+                
+                i++;
+            }
+
+            if (!iterations) {  
+                // Panggil path dari file yang tdk punya extension sesuai    
+                sprintf(prevPath, "%s/%s", currentPath, filename);
+                sprintf(newPath, "%s/%s/%s", pathToCategorized, "categorized/other", filename); 
+                rename(prevPath, newPath);
+            }
+        }
+    }
+    closedir(dir);
+}
+
 
 int getExtensions(){
-    
+    // Buka file extensions.txt
     FILE* fp = fopen("extensions.txt", "r");
-
-    while (fgets(ext_list[ext_count], extensionsLength, fp)) {
-      ext_list[ext_count][strcspn(ext_list[ext_count], "\r\n")] = 0; // hapus karakter '\r' dan '\n'
+    while (fgets(extensionsLists[ext_count], extensionsLength, fp)) {
+    //Buat iterasi dari isi extensions.txt untuk menentukan file yang harus dibuat 
+      extensionsLists[ext_count][strcspn(extensionsLists[ext_count], "\r\n")] = 0; 
       ext_count++;
       if (ext_count >= extensions) {
         return 1;
       }
     }
-
     fclose(fp);
-
-}
-
-void categorizeFiles(char *currentPath, char ext_list[extensions][extensionsLength], int ext_count) {
-    DIR *dir = opendir(currentPath);
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_DIR) {
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-                continue;
-            }
-            char sub_currentPath[1000];
-            sprintf(sub_currentPath, "%s/%s", currentPath, entry->d_name);
-            categorizeFiles(sub_currentPath, ext_list, ext_count); 
-        } else if (entry->d_type == DT_REG) {
-            char* filename = entry->d_name;
-            char filenames[1000];
-
-            // konversi nama file ke lowercase
-            for (int j = 0; filename[j] != '\0'; j++) {
-                filenames[j] = tolower(filename[j]);
-            }
-            filenames[strlen(filename)] = '\0';
-
-            int iterations = 0;
-            for (int i = 0; i < ext_count; i++) {
-                int result = mkdir(ext_list[i], 0777);
-            
-                char ext[extensionsLength + 2]; // tambahkan 2 karakter untuk "*.ext" dan terminasi string
-                sprintf(ext, "*.%s", ext_list[i]);
-                
-                if (fnmatch(ext, filenames, 0) == 0) {
-                
-                    sprintf(prevPath, "%s/%s", currentPath, filename); // membuat path lengkap file asli
-                    sprintf(newPath, "%s/%s/%s/%s", pathToCategorized, "categorized", ext_list[i], filename); // membuat path lengkap file tujuan
-
-
-                    rename(prevPath, newPath); 
-                    iterations = 1;
-                    break;
-                }
-            }
-
-            if (!iterations) {                 
-                sprintf(prevPath, "%s/%s", currentPath, filename); 
-                sprintf(newPath, "%s/%s/%s", pathToCategorized, "categorized/other", filename); 
-
-                rename(prevPath, newPath);
-            }
-        }
-    }
-
-    closedir(dir);
 }
 
 
+int countCategorize(char *currentPath) {
+DIR *dir = opendir(currentPath);
+int count = 0;
+struct dirent *entry;
 
-int count_files(char *currentPath) {
-    DIR *dir = opendir(currentPath);
-    int count = 0;
-
-     if (strstr(currentPath, "categorized") == NULL) { // tambahkan kondisi untuk hanya menghitung isi dari direktori categorized
+// Check apakah direktori saat ini merupakan direktori categorized
+switch (strstr(currentPath, "categorized") == NULL) {
+    case 1:
         return 0;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_DIR) {
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, "/files/") == 0 ) {
-                continue;
+    default:
+        while ((entry = readdir(dir)) != NULL) {
+            if (entry->d_type == DT_DIR) {
+                // Abaikan direktori berikut
+                switch (strcmp(entry->d_name, ".")) {
+                    case 0:
+                        continue;
+                    default:
+                        break;
+                }
+                switch (strcmp(entry->d_name, "..")) {
+                    case 0:
+                        continue;
+                    default:
+                        break;
+                }
+                switch (strcmp(entry->d_name, "/files/")) {
+                    case 0:
+                        continue;
+                    default:
+                        break;
+                }
+                sprintf(sub_currentPath, "%s/%s", currentPath, entry->d_name);
+                int sub_count = countCategorize(sub_currentPath); // memanggil diri sendiri secara rekursif
+                printf("%s: %d\n", sub_currentPath, sub_count);
+                count += sub_count;
+            } else {
+                count++;
             }
-            char sub_currentPath[1000];
-            sprintf(sub_currentPath, "%s/%s", currentPath, entry->d_name);
-            int sub_count = count_files(sub_currentPath); // memanggil diri sendiri secara rekursif
-            printf("%s: %d\n", sub_currentPath, sub_count);
-            count += sub_count;
-        } else {
-            count++;
         }
-    }
-
-    closedir(dir);
-
-    return count;
+        closedir(dir);
+        printf("\nJumlah isi tiap direktori: %d", &count);
+        return count;
+}
 }
 
 int main() {
@@ -127,18 +147,19 @@ int main() {
 
     getExtensions();
     
-    
+    // Masuk ke direktori soal 4
     chdir("/home/reyhanqb/Documents/Shift 3/soal4");
-    int f = mkdir("categorized", 0777);
+    mkdir("categorized", 0777);
     
+    // Masuk kedalam direktori categorized
     chdir("/home/reyhanqb/Documents/Shift 3/soal4/categorized/");
     
-    int other_result = mkdir("other", 0777); // tambahkan untuk membuat folder 'other'
+    // Buat direktori other
+    mkdir("other", 0777); 
 
-    categorizeFiles(currentPath, ext_list, ext_count);
+    categorizeFiles(currentPath, extensionsLists, ext_count);
 
-    printf("\nJumlah isi tiap direktori:\n");
-    count_files(pathToCategorized);
+    countCategorize(pathToCategorized);
 
     return 0;
 }
