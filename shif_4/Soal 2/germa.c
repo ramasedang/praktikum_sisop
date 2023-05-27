2ce8872873a4022095a79431bc087148cd7cef4e
aloybm <manullangaloysius@gmail.com>
2023-05-27 19:24:36 +0700

    no2 fix

--- shif_4/Soal 2/germa.c
+++ shif_4/Soal 2/germa.c
@@ -13,27 +13,37 @@
 static const char *dirpath = "/home/rxpkisoviet/Documents";
 static const char *logpath = "/home/rxpkisoviet/Documents/logmucatatsini.txt";
 
-void log_write(char *lvl, char *message, ...);
-int is_restricted(const char *path);
-static int xmp_getattr(const char *path, struct stat *stbuf);
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
-static int xmp_rename(const char *from, const char *to);
-static int xmp_unlink(const char *path);
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
-static int xmp_mkdir(const char *path, mode_t mode);
-static int xmp_rmdir(const char *path);
-static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi);
+//membuat file log
+void fuse_log_write(char *lvl, char *message, ...);
+//melakukan pengecekan dengan kata restricted
+int fuse_is_restricted(const char *path);
+//mendapatkan file atribut
+static int fuse_xmp_getattr(const char *path, struct stat *stbuf);
+//membaca direktori
+static int fuse_xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
+//mengubah nama file atau direktori
+static int fuse_xmp_rename(const char *from, const char *to);
+//menghapus file
+static int fuse_xmp_unlink(const char *path);
+//membaca file
+static int fuse_xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
+//membuat folder
+static int fuse_xmp_mkdir(const char *path, mode_t mode);
+//menghapus folder
+static int fuse_xmp_rmdir(const char *path);
+//membuat file
+static int fuse_xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi);
 
-
-static struct fuse_operations xmp_oper = {
-    .getattr = xmp_getattr,
-    .readdir = xmp_readdir,
-    .rename = xmp_rename,
-    .unlink = xmp_unlink,
-    .read = xmp_read,
-    .mkdir = xmp_mkdir,
-    .rmdir = xmp_rmdir,
-    .create = xmp_create,
+//operasi fuse
+static struct fuse_operations fuse_xmp_oper = {
+    .getattr = fuse_xmp_getattr,
+    .readdir = fuse_xmp_readdir,
+    .rename = fuse_xmp_rename,
+    .unlink = fuse_xmp_unlink,
+    .read = fuse_xmp_read,
+    .mkdir = fuse_xmp_mkdir,
+    .rmdir = fuse_xmp_rmdir,
+    .create = fuse_xmp_create,
 };
 
 
@@ -41,17 +51,19 @@ static struct fuse_operations xmp_oper = {
 int main(int argc, char *argv[])
 {
     umask(0);
-
+    //memanggil fungsi fuse
     return fuse_main(argc, argv, &xmp_oper, NULL);
 }
 
-void log_write(char *lvl, char *message, ...)
+//membuat log 
+void fuse_log_write(char *lvl, char *message, ...)
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     FILE *f = fopen(logpath, "a");
     va_list args;
     va_start(args, message);
+    //memprnt time
     fprintf(f, "%s::%02d/%02d/%04d-%02d:%02d:%02d::", lvl, tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
     vfprintf(f, message, args);
     fprintf(f, "\n");
@@ -59,7 +71,8 @@ void log_write(char *lvl, char *message, ...)
     va_end(args);
 }
 
-int is_restricted(const char *path)
+//mengecek kata restricted
+int fuse_is_restricted(const char *path)
 {
     // Check if a path contains the word "restricted"
     if (strstr(path, "restricted") != NULL) {
@@ -68,8 +81,8 @@ int is_restricted(const char *path)
     return 0; // Access granted
 }
 
-
-static int xmp_getattr(const char *path, struct stat *stbuf)
+//mengambil file atribut
+static int fuse_xmp_getattr(const char *path, struct stat *stbuf)
 {
     int res;
     char fpath[1000];
@@ -79,12 +92,13 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
     res = lstat(fpath, stbuf);
 
     if (res == -1) 
-        return -errno;
+        return -errno; //akses tidak diizinkan
 
     return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+//membaca direktori
+static int fuse_xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
     char fpath[1000];
 
@@ -125,51 +139,53 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
     return 0;
 }
 
-static int xmp_rename(const char *from, const char *to)
+//mengubah nama file dan direktori
+static int fuse_xmp_rename(const char *from, const char *to)
 {
     char fpath_from[1000], fpath_to[1000];
     sprintf(fpath_from, "%s%s", dirpath, from);
     sprintf(fpath_to, "%s%s", dirpath, to);
 
     // Check if the source name contains "restricted"
-    if (is_restricted(fpath_from)) {
+    if (fuse_is_restricted(fpath_from)) {
         // Check if the destination name does not contain "restricted" or "bypass"
         if (strstr(to, "restricted") != NULL || strstr(to, "bypass") == NULL) {
-            log_write("FAILED", "RENAME::rxpkisoviet-Rename from %s to %s", fpath_from, fpath_to);
+            fuse_log_write("FAILED", "RENAME::rxpkisoviet-Rename from %s to %s", fpath_from, fpath_to);
             return -EPERM; // Operation not permitted
         }
     }
 
     int res = rename(fpath_from, fpath_to);
     if (res == -1) {
-        log_write("FAILED", "RENAME::rxpkisoviet-Rename from %s to %s", fpath_from, fpath_to);
+        fuse_log_write("FAILED", "RENAME::rxpkisoviet-Rename from %s to %s", fpath_from, fpath_to);
         return -errno;
     }
-    log_write("SUCCESS", "RENAME::rxpkisoviet-Rename ffrom %s to %s", fpath_from, fpath_to);
+    fuse_log_write("SUCCESS", "RENAME::rxpkisoviet-Rename ffrom %s to %s", fpath_from, fpath_to);
     return 0;
 }
 
 
-
-static int xmp_unlink(const char *path)
+//menghapus file
+static int fuse_xmp_unlink(const char *path)
 {
     char fpath[1000];
     sprintf(fpath, "%s%s", dirpath, path);
-
-    if (is_restricted(fpath)){
+    //mengecek kata restricted
+    if (fuse_is_restricted(fpath)){
         log_write("FAILED", "RMFILE::rxpkisoviet-Remove file %s.", fpath);
         return -EPERM; // Operation not permitted
     }
     int res = unlink(fpath);
     if (res == -1){
-        log_write("FAILED", "RMFILE::rxpkisoviet-Remove file %s.", fpath);
+        fuse_log_write("FAILED", "RMFILE::rxpkisoviet-Remove file %s.", fpath);
         return -errno;
     }
-    log_write("SUCCESS", "RMFILE::rxpkisoviet-Remove file %s.", fpath);
+    fuse_log_write("SUCCESS", "RMFILE::rxpkisoviet-Remove file %s.", fpath);
     return 0;
 }
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+//membaca file
+static int fuse_xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
     char fpath[1000];
 
@@ -199,13 +215,14 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
     return res;
 }
 
-static int xmp_mkdir(const char *path, mode_t mode)
+//membuat direktori
+static int fuse_xmp_mkdir(const char *path, mode_t mode)
 {
     char fpath[1000];
 
     // Check if the path contains the word "restricted"
-    if (is_restricted(path)) {
-        log_write("FAILED", "MKDIR::rxpkisoviet-Create %s.", fpath);
+    if (fuse_is_restricted(path)) {
+        fuse_log_write("FAILED", "MKDIR::rxpkisoviet-Create %s.", fpath);
         return -EPERM; // Operation not permitted
     }
 
@@ -213,48 +230,52 @@ static int xmp_mkdir(const char *path, mode_t mode)
 
     int res = mkdir(fpath, mode);
     if (res == -1) {
-        log_write("FAILED", "MKDIR::rxpkisoviet-Create directory %s.", fpath);
+        fuse_log_write("FAILED", "MKDIR::rxpkisoviet-Create directory %s.", fpath);
         return -errno;
     }
-    log_write("SUCCESS", "MKDIR::rxpkisoviet-Create directory %s.", fpath);
+    fuse_log_write("SUCCESS", "MKDIR::rxpkisoviet-Create directory %s.", fpath);
     return 0;
 }
 
-static int xmp_rmdir(const char *path)
+//menghapus direktori
+static int fuse_xmp_rmdir(const char *path)
 {
     char fpath[1000];
     sprintf(fpath, "%s%s", dirpath, path);
-    if (is_restricted(fpath)) {
-        log_write("FAILED", "RMDIR::rxpkisoviet-Remove directory %s.", fpath);
+    //mengecek kata restricted
+    if (fuse_is_restricted(fpath)) {
+        fuse_log_write("FAILED", "RMDIR::rxpkisoviet-Remove directory %s.", fpath);
         return -EPERM; // Operation not permitted
     }
     int res = rmdir(fpath);
     if (res == -1) {
-        log_write("FAILED", "RMDIR::rxpkisoviet-Remove directory %s.", fpath);
+        fuse_log_write("FAILED", "RMDIR::rxpkisoviet-Remove directory %s.", fpath);
         return -errno;
     }
-    log_write("SUCCESS", "RMDIR::rxpkisoviet-Remove directory %s.", fpath);
+    fuse_log_write("SUCCESS", "RMDIR::rxpkisoviet-Remove directory %s.", fpath);
     return 0;
 }
-static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+
+//membuat file
+static int fuse_xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi)
 {
     char fpath[1000];
     sprintf(fpath, "%s%s", dirpath, path);
 
     // Check if the path contains the word "restricted"
-    if (is_restricted(fpath)) {
-        log_write("FAILED", "CREATE::rxpkisoviet-Create file %s.", fpath);
+    if (fuse_is_restricted(fpath)) {
+        fuse_log_write("FAILED", "CREATE::rxpkisoviet-Create file %s.", fpath);
         return -EPERM; // Operation not permitted
     }
 
     int res = open(fpath, fi->flags, mode);
     if (res == -1) {
-        log_write("FAILED", "CREATE::rxpkisoviet-Create file %s.", fpath);
+        fuse_log_write("FAILED", "CREATE::rxpkisoviet-Create file %s.", fpath);
         return -errno;
     }
     fi->fh = res;
 
-    log_write("SUCCESS", "CREATE::rxpkisoviet-Create file %s.", fpath);
+    fuse_log_write("SUCCESS", "CREATE::rxpkisoviet-Create file %s.", fpath);
     return 0;
 }
 
